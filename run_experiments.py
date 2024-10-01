7c4bf5464de81e11e0fe08a11ee8fbd648b9f642
Eduardo Aguilar <earguila00@hotmail.com>
2024-10-01 12:39:03 +0100

    run_experiments file created

---
+++ run_experiments.py
@@ -0,0 +1,13 @@
+from experiments_scripts.train_GNN import train_model
+from options.base_options import BaseOptions
+
+def run():
+    opt = BaseOptions()
+    opt = opt.parse()
+
+    if opt.train_GNN:
+        train_model()
+
+
+if __name__ == '__main__':
+    run()
