1579d0310b55b06dcd6d1de3766994f28074bb44
Eduardo Aguilar <earguila00@hotmail.com>
2024-10-04 11:46:24 +0100

    XAI experiments created

--- trials.py
+++ trials.py
@@ -1,12 +1,13 @@
 import torch
 from torch_geometric.loader import DataLoader
 import json
-import numpy as np
-from rdkit import Chem
+
 import random
 from options.base_options import BaseOptions
-from utils.XAI_utils import calculate_attributions
+from utils.XAI_utils import calculate_attributions, get_attrs_atoms, calculate_XAI_metrics, create_XAI_report
+import os
 from icecream import ic
+
 
 
 def run():
@@ -18,81 +19,51 @@ def run():
     random.seed(opt.global_seed)
 
     # Load the dataset and model from experiment
-    exp_dir = '3MR/graphsage'
-    train_loader = DataLoader(torch.load("{}/train_loader.pth".format(exp_dir)).dataset[:50])
-    test_loader = DataLoader(torch.load("{}/test_loader.pth".format(exp_dir)).dataset[:10])
+    exp_dir = f'{opt.exp_name}/{opt.filename[:-4]}/{opt.network_name}/results_model'
+    train_loader = DataLoader(torch.load("{}/train_loader.pth".format(exp_dir)).dataset[:30])
+    test_loader = DataLoader(torch.load("{}/test_loader.pth".format(exp_dir)).dataset[:30])
+
+    log_dir = f'{opt.exp_name}/{opt.filename[:-4]}/{opt.network_name}/results_XAI/{opt.XAI_algorithm}'
+    os.makedirs(log_dir, exist_ok=True) 
+    os.makedirs(f'{log_dir}/mols', exist_ok=True)
 
 
-    #calculate_attributions(opt, train_loader)
-
-    with open('node_masks_test.json') as f:
-        data = json.load(f)
-
-    
-
-    mol_attrs = {}
-    
-    for outer_key, outer_value in data.items():
-        dir, mag = None, None
-        for inner_key, inner_value in outer_value.items():
-            value = np.array(inner_value)
-            value /= np.abs(np.max(value))
-            #ic(inner_key, value.shape)
-            if inner_key == 'GNNExplainer' or inner_key == 'Saliency':
-                if not isinstance(mag, np.ndarray):
-                    mag = value
-                else:
-                    mag += value
-            else:
-                if not isinstance(dir, np.ndarray):
-                    dir = value
-                else:
-                    dir += value
-        
-        sign = np.sign(dir)
-        dir = np.abs(dir)
-        mag = dir + mag
-        mag = np.sum(mag, axis=1) # sum over the columns to get the overall importance of each atom
-        mag /= np.max(mag) # normalize the importance values from 0 to 1
-        #mag = mag * sign
-
-        mag = np.where(mag < 0.5, 0, 1) # thresholding to map atoms to either important or not 
-
-        mol_attrs[outer_key] = mag
-
-    
-    pattern = Chem.MolFromSmarts('[*]1[*][*]1')
-    for smiles, attrs in mol_attrs.items():
-        mol = Chem.MolFromSmiles(smiles) 
-        matches = mol.GetSubstructMatches(pattern)
-
-        acc = 0
-
-        indexes = set().union(*matches)
-        ic(indexes)
+    # Calculate the attributions and generate JSON files if they do not exist for both the training and test set
+    if os.path.exists(f'{log_dir}/node_masks_train.json'):
+        with open(f'{log_dir}/node_masks_train.json') as f:
+            data_train = json.load(f)
+    else:
+        data_train = calculate_attributions(opt = opt, loader = train_loader, XAI_algorithm=opt.XAI_algorithm)
+        with open(f'{log_dir}/node_masks_train.json', 'w') as f:
+            json.dump(data_train, f, indent=4)
+    if os.path.exists(f'{log_dir}/node_masks_test.json'):
+        with open(f'{log_dir}/node_masks_test.json') as f:
+            data_test = json.load(f)
+    else:
+        data_test = calculate_attributions(opt = opt, loader = test_loader, XAI_algorithm=opt.XAI_algorithm)
+        with open(f'{log_dir}/node_masks_test.json', 'w') as f:
+            json.dump(data_test, f, indent=4)
 
 
-        for i, attr in enumerate(attrs):
-            ic(i, attr)
-            if attr == 1:
-                if i in indexes:
-                    acc += 1
-                elif i not in indexes:
-                    pass
-            elif attr == 0:
-                if i in indexes:
-                    pass
-                elif i not in indexes:
-                    acc += 1
-            
-            #ic(acc)
-        
-        ic(acc)
-        ic(len(attrs))
-                    
-        ic(smiles, acc/len(attrs))
-        break
+    # Calculate the best threshold for the test set by using the training set
+    mol_attrs_train = get_attrs_atoms(data_train)
+    threshold_results = {}
+    for threshold in range(0, 100, 5):
+        accuracies_train = []
+        results_train = calculate_XAI_metrics(opt, mol_attrs=mol_attrs_train, exp_dir=exp_dir, threshold=threshold/100, save_results=False)
+        for _, value in results_train.items():
+            accuracies_train.append(value['Accuracy'])
+        mean_accuracy = sum(accuracies_train)/len(accuracies_train)
+        threshold_results[threshold/100] = mean_accuracy
+    best_threshold = max(threshold_results, key=threshold_results.get)
+    print(f'The best threshold is {best_threshold} with an accuracy of {threshold_results[best_threshold]}')
 
+
+    # Calculate the metrics for the test set using the best threshold found
+    mol_attrs = get_attrs_atoms(data_test)
+    results_test = calculate_XAI_metrics(opt, mol_attrs=mol_attrs, exp_dir=exp_dir, threshold=best_threshold, save_results=True, logdir=f'{log_dir}')
+
+    create_XAI_report(opt, best_threshold, threshold_results, results_test, log_dir)
 
 
         
