acb6c04c942f7d4b9d048ede6611f01e82cf7009
Eduardo Aguilar <earguila00@hotmail.com>
2024-10-04 11:54:01 +0100

    train test splitting code added

--- experiments_scripts/train_GNN.py
+++ experiments_scripts/train_GNN.py
@@ -2,40 +2,33 @@ import torch
 from torch_geometric.loader import DataLoader
 from utils.model_utils import train_network, eval_network, network_report
 from data.mol_instance import molecular_graph
-from options.base_options import BaseOptions
 from call_methods import make_network
+from icecream import ic
 
-def train_model():
-
-    # Parse the options
-    opt = BaseOptions()
-    opt = opt.parse()
+def train_model(opt):
 
     # Set the device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load the dataset
-    mols = molecular_graph(opt = opt, filename = '3MR.csv', root = 'data/datasets/3MR')
+    mols = molecular_graph(opt = opt, filename = opt.filename, root = opt.root)
+
+    
+    train_indices = [i for i, s in enumerate(mols.set) if s == 'train']
+    test_indices = [i for i, s in enumerate(mols.set) if s == 'test']
+
+    train_dataset = mols[train_indices]
+    test_dataset = mols[test_indices]
+
+    # Make the dataloaders
+    train_set = DataLoader(train_dataset, batch_size=opt.batch_size, shuffle=True)
+    test_set = DataLoader(test_dataset, batch_size=opt.batch_size, shuffle=False)
 
     # Make the network
-    model = make_network(network_name='graphsage', 
+    model = make_network(network_name=opt.network_name, 
                          opt=opt, 
                          n_node_features=mols.num_node_features, 
                          n_edge_features=mols.num_edge_features).to(device)
-
-    train_set = []
-    test_set = []
-
-    for mol in mols:
-        if mol.set == 'train':
-            train_set.append(mol)
-        elif mol.set == 'test':
-            test_set.append(mol)
-
-
-    # Make the dataloaders
-    train_set = DataLoader(train_set, batch_size=opt.batch_size, shuffle=True)
-    test_set = DataLoader(test_set, batch_size=opt.batch_size, shuffle=False)
     
     train_list,  test_list = [], []
 
@@ -56,7 +49,7 @@ def train_model():
         test_list.append(test_loss)
 
     network_report(
-                exp_name=mols.filename[:-4],
+                exp_name=f'{opt.exp_name}/{mols.filename[:-4]}',
                 loaders=(train_set, test_set),
                 loss_lists=(train_list, test_list),
                 save_all=True,
@@ -64,5 +57,3 @@ def train_model():
             )
 
 
-if __name__ == '__main__':
-    train_model()
