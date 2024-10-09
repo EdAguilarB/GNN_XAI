4b9fede1e264579f01e1e487ad0f84629c0f7a8f
Eduardo Aguilar <earguila00@hotmail.com>
2024-10-09 13:23:50 +0100

    training plot function corrected

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -14,19 +14,19 @@ def create_training_plot(df, save_path):
     df = pd.read_csv(df)
 
     epochs = df.iloc[:,0]
-    train_loss = df.iloc[:,1]
-    #val_loss = df.iloc[:,2]
-    test_loss = df.iloc[:,2]
+    train_loss = df.iloc[:,2]
+    val_loss = df.iloc[:,3]
+    test_loss = df.iloc[:,4]
 
-    #min_val_loss_epoch = epochs[val_loss.idxmin()]
+    min_val_loss_epoch = epochs[val_loss.idxmin()]
 
     # Create a Matplotlib figure and axis
     plt.figure(figsize=(10, 6), dpi=300)  # Adjust the figure size as needed
     plt.plot(epochs, train_loss, label='Train Loss', marker='o', linestyle='-')
-    #plt.plot(epochs, val_loss, label='Validation Loss', marker='o', linestyle='-')
+    plt.plot(epochs, val_loss, label='Validation Loss', marker='o', linestyle='-')
     plt.plot(epochs, test_loss, label='Test Loss', marker='o', linestyle='-')
 
-    #plt.axvline(x=min_val_loss_epoch, color='gray', linestyle='--', label=f'Min Validation Epoch ({min_val_loss_epoch})')
+    plt.axvline(x=min_val_loss_epoch, color='gray', linestyle='--', label=f'Min Validation Epoch ({min_val_loss_epoch})')
 
     # Customize the plot
     plt.xlabel('Epochs')
